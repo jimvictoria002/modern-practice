06b421b6e1fd7898f03f5a77943629603faba246
Jimuel Hayan Victoria <138399761+jimvictoria002@users.noreply.github.com>
2024-06-24 19:35:03 +0800

    fix seeder

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -18,11 +18,15 @@ class OrderFactory extends Factory
      */
     public function definition(): array
     {
+        $customer = Customer::inRandomOrder()->first();
+        $drink = Drink::inRandomOrder()->first();
+        $quantity = $this->faker->numberBetween(1, 5);
+        $total = $drink->price * $quantity;
         return [
-            'customer_id' => Customer::inRandomOrder()->first()->id,
-            'drink_id' => Drink::inRandomOrder()->first()->id,
-            'quantity' => $this->faker->numberBetween(1, 5), // Example: random quantity between 1 and 5
-            'total' => $this->faker->randomFloat(2, 10, 100), // Example: random total amount between 10 and 100 with 2 decimal places
+            'customer_id' => $customer->id,
+            'drink_id' => $drink->id,
+            'quantity' => $quantity, // Example: random quantity between 1 and 5
+            'total' => $total, // Example: random total amount between 10 and 100 with 2 decimal places
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(20000)->create();
-        Drink::factory(3)->create();
-        Customer::factory(5)->create();
-        Order::factory(100)->create();
+        // Drink::factory(3)->create();
+        // Customer::factory(5)->create();
+        Order::factory(10)->create();
 
     }
 }
